2912d71d5b475dcd18adfd9b47f55de6b5b892ae
Luke Fuller <lukefuller@Lukes-MacBook-Pro.local>
2022-02-22 19:00:11 -0500

    Upload initial File Path

---
+++ Flask-API/PostGreSQL/DataSetCleaning/psql_schema.sql
@@ -0,0 +1,20 @@
+DROP TABLE athletes;
+
+CREATE TABLE athletes (
+	pkid INT PRIMARY KEY NOT NULL,
+	id INT NOT NULL,
+	name VARCHAR NOT NULL,
+	sex VARCHAR(1) NOT NULL,
+	age FLOAT,
+	height FLOAT,
+	weight FLOAT,
+	country VARCHAR NOT NULL,
+	noc VARCHAR NOT NULL,
+	games VARCHAR NOT NULL,
+	year INT NOT NULL,
+	season VARCHAR NOT NULL,
+	city VARCHAR NOT NULL,
+	sport VARCHAR NOT NULL,
+	event VARCHAR NOT NULL,
+	medal VARCHAR
+);
---
+++ Flask-API/config_postgres.py
@@ -0,0 +1,2 @@
+password = "FlynnsArcade#19707"
+database_name = "future_data_letes"
